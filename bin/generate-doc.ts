385376284d26c5d7ed535ab8d094b9a94a678f9f
Renaud Guyon <reno.guyon@gmail.com>
2022-07-05 10:55:41 -0400

    Update DocGenerator import

--- bin/generate-doc.ts
+++ bin/generate-doc.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env ts-node
 import * as path from 'path';
-import { DocGenerator } from '@extcore/server';
+import { DocGenerator } from '@extcore/server/dist/tools';
 
 const docGenerator = new DocGenerator({
   handlerPath: path.join(__dirname, '../src/http/handlers'),
